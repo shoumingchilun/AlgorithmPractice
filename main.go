f76e147ebb8ab9ee9658c1fa500a0f8d54b2e5a0
齿轮 <2265501251@qq.com>
2022-11-08 11:33:44 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"workSpace/mergoSort"
+)
+
+func main() {
+	fmt.Println(mergoSort.MergoSort([]int{3, 4, 2, 1, 5, 7, 8, 4, 7}))
+}
---
+++ mergoSort/mergeSort.go
@@ -0,0 +1,40 @@
+package mergoSort
+
+func MergoSort(numbers []int) (orderedNumbers []int) {
+	if len(numbers) <= 1 {
+		return numbers
+	} else {
+		mid := len(numbers) / 2
+		left := MergoSort(numbers[:mid])
+		right := MergoSort(numbers[mid:])
+		orderedNumbers = Merge(left, right)
+		return
+	}
+}
+
+func Merge(left []int, right []int) (result []int) {
+	result = make([]int, len(left)+len(right))
+	i, j, k := 0, 0, 0
+	for j < len(left) && k < len(right) {
+		if left[j] >= right[k] {
+			result[i] = left[j]
+			i++
+			j++
+		} else {
+			result[i] = right[k]
+			i++
+			k++
+		}
+	}
+	for j < len(left) {
+		result[i] = left[j]
+		j++
+		i++
+	}
+	for k < len(right) {
+		result[i] = right[k]
+		k++
+		i++
+	}
+	return result
+}
